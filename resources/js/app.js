1dca7355cffe96fef9e5852cc552e5155f820cc3
bouss <bousswuil@gmail.com>
2021-12-19 05:01:39 +0100

    (rework) menu de la homepage simplifier

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,15 @@
 import '../css/app.css'
 
 import HeaderMenu from "./components/HeaderMenu"
+import HeaderMenuButton from './components/HeaderMenuButton'
+import Sticky from './components/Sticky'
 
-HeaderMenu.create({
-  openBtnSelector: '.js-open-menu',
+
+// openBtnSelector: '.js-open-menu',
+// 
+// 
+HeaderMenuButton.create({
+  openButtonSelector: '.header-menu-button',
   menuSelector: '.js-close-menu',
   closeElementSelector: '.js-menu-hidden',
 })
--- resources/js/components/HeaderMenuButton.js
+++ resources/js/components/HeaderMenuButton.js
@@ -3,24 +3,53 @@ import Sticky from "./Sticky"
 export default class HeaderMenuButton {
   /**
    * @param {{
-   * buttonSelector:string
+   * openButtonSelector:string,
+   * menuSelector:string,
+   * closeElementSelector:string
    * }} options options du boutton dans le menu
    */
   constructor(options) {
-    this.$btn = document.querySelector(options.buttonSelector)
+    this.$openBtn = document.querySelector(options.openButtonSelector)
+    this.$menu = document.querySelector(options.menuSelector)    
+    this.$closeBtn = this.$menu.querySelector(options?.closeBtnSelector || '#button-close')
+    this.$closeElement = document.querySelector(options?.closeElementSelector)
 
-    if (!this.$btn) throw new Error('selecteur du boutton de menu non defini')
+    console.log(this.$closeBtn)
 
-    this.stickyMenu = null
+    if (!this.$openBtn) throw new Error('selecteur du boutton de menu non defini')
+    if (!this.$menu) throw new Error('element reprseting menu not dinfined, please set correct selector to "menuSelector" option')
+    if (!this.$closeBtn) throw new Error('button element use to close menu not define, please set correct selector "closeBtnSelector" option')
+    if (!this.$closeElement) throw new Error('background element use to close menu not define, please set correct selector "closeElementSelector" option')
 
     this.init()
   }
 
   init() {
-    this.stickyMenu = Sticky.define({
-      element: this.$btn,
+    this.$openBtn.addEventListener('click', this.openMenu.bind(this))
+
+    this.$closeBtn.addEventListener('click', this.closeMenu.bind(this))
+
+    Sticky.define({
+      element: this.$openBtn,
       scrollValue: 45,
       activeClass: 'scroll'
+    })
+  }
+
+
+  openMenu() {
+    this.$menu.classList.replace('hide', 'show')
+    this.$closeElement.style.setProperty('display', 'block')
+    this.$menu.addEventListener('animationend', () => {
+      this.$closeElement.addEventListener('click', this.closeMenu.bind(this), { once: true })
+    })
+  }
+
+  closeMenu() {
+    this.$closeElement.style.setProperty('display', 'none')
+    this.$menu.classList.replace('show', 'leave')
+    this.$menu.addEventListener('animationend', (e) => {
+      this.$menu.classList.replace('leave', 'hide')
     })
   }
 
