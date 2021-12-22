4942073df4a7764de41d5189181417190bbd411d
bouss <bousswuil@gmail.com>
2021-12-22 17:02:06 +0100

    (update) rendu de la page de recherche des prestataire

--- app/Controllers/Http/SearchesController.ts
+++ app/Controllers/Http/SearchesController.ts
@@ -6,6 +6,7 @@ import Arrondissement from 'App/Models/Arrondissement'
 import Quater from 'App/Models/Quater'
 import ServiceProvider from 'App/Models/ServiceProvider'
 import Adress from 'App/Models/Adress'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 // interface searchQuery {
 //   job: string,
@@ -17,54 +18,11 @@ export default class SearchesController {
   private ORDER = 'score'
 
   public async index({ request, view, session }: HttpContextContract) {
-    const qs = request.qs()
-    const page = Number.parseInt(qs.page, 10) || 1
-    const jobId = Number.parseInt(qs.job, 10) || 0
-    
-    let cityId = Number.parseInt(qs.city, 10) || 0
-    let arrondissementId = Number.parseInt(qs.arrondissement, 10) || 0
-    let quaterId = Number.parseInt(qs.quater, 10) || 0
-    
     const jobs = await Job.query().orderBy('name', 'asc')
     let cities = await City.query().orderBy('name', 'asc')
-    let arrondissements:Arrondissement[] = []
-    let quaters:Quater[] = []
-    let serviceProviders: ModelPaginatorContract<ServiceProvider>|null = null 
-
-    const city = await City.find(cityId) || {id: 0}
-
-    const arr = await Arrondissement.query().where('city_id', city.id)
-      .where('id', arrondissementId).first()|| {id: 0}
-
-    const quater = await Quater.query().where('city_id', city.id)
-      .where('arrondissement_id', arr.id).where('id', quaterId).first()
-
-    if(city) {
-      arrondissements = await Arrondissement.query().where('city_id', city.id)
-    }
-
-    if(arr) {
-      const id = city ? city.id : 0 
-      quaters = await Quater.query().where('city_id', id).where('arrondissement_id', arr.id)
-    }
-
-    let query:ModelQueryBuilderContract<typeof ServiceProvider>|null = null
-
-    const job = await Job.find(jobId)
-
-    if(job) {
-      query = ServiceProvider.query().where('jobs_id', job.id)
-    }
-    else {
-      query = ServiceProvider.query()
-    }
-
-
-
-    const data = await query.paginate(page, this.LIMIT)
-
+    
     return view.render('search/index', {
-      qs, jobs, cities, arrondissements, quaters
+      jobs, cities
     })
   }
 
