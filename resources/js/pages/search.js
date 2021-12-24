cea742cf657a3dfd9f6805da054bd05f45f133d9
bouss <bousswuil@gmail.com>
2021-12-24 20:10:46 +0100

    (update) stylisation du boutton voir plus

--- resources/js/pages/search.js
+++ resources/js/pages/search.js
@@ -11,11 +11,10 @@ const $limitInput = document.querySelector('#limit-input')
 const $limit = document.querySelector("#search-limit")
 
 const fields = Array.from($form.querySelectorAll('.search-field > select'))
-const LAST_QUERY = {}
-const ASSETS_URL = 'http://localhost:8000/assets/'
 
 $form.addEventListener('change', (e) => {
   const id = e.target.id
+
   updateLimitValue({target: $limitInput})
   if(id === 'city') {
     fields.filter(field => field.id === 'arrondissement' || field.id === 'quater')
@@ -35,6 +34,8 @@ $form.addEventListener('change', (e) => {
     $form.submit()
   }
 })
+
+$form.addEventListener('submit', startFormLoader)
 
 $range.addEventListener('pointerup', updateLimitValue)
 $range.addEventListener('pointerdown', e => {
@@ -87,6 +88,14 @@ function more() {
 
 /**
  * 
+ * @param {SubmitEvent} e 
+ */
+function startFormLoader(e) {
+  $form.classList.add('loading')
+}
+
+/**
+ * 
  * @param {PointerEvent} e 
  */
 function updateLimitValue(e) {
@@ -113,53 +122,4 @@ function updateLimitValue(e) {
     $limit.value = $range.value =  value
   }
   
-}
-
-/**
- * 
- * @param {number} value 
- */
-function fillLimitFields(value) {
-  $limit.value = $limitInput.value = $range.value = value
-}
-
-/**
- * 
- * @param {SubmitEvent} e 
- */
-function handleFormChange(e) {
-  e.preventDefault()
-  const id = e.target.id 
-  LAST_QUERY[id] = e.target.value
-  if( id === 'job' || id === 'quater' ) {
-    console.log('find sp')
-    getServiceProviders(LAST_QUERY)
-    return
-  }
-
-  FetchApi.getForm(LAST_QUERY).then(data => {
-    $form.innerHTML = data
-  })
-  .then(() => getServiceProviders(LAST_QUERY))
-
-}
-
-function getServiceProviders(query) {
-  console.log(query)
-  FetchApi.getServiceProviders(query).then(data => addServiceProviders(data))
-}
-
-function addServiceProviders(data) {
-  const content = $service_provider_card_template.content.firstElementChild
-  let res = ''
-  data.forEach(d => {
-    const clone = content.cloneNode(true)
-    const photo_url = d.photo ? ASSETS_URL + d.photo : '/favicon.jpg'
-    clone.querySelector('#photo').setAttribute('src', photo_url) 
-    clone.querySelector('#job').innerHTML = d.lastname
-    clone.querySelector('#devis').setAttribute('href', '/devis/client/'+ d.id)
-    res += clone.innerHTML
-  })
-
-  $service_provider_results.innerHTML = res
 }
