fa4c7950e4e9d0a0f5d72362710f7e262ab2b225
wuil242 <bousswuil@gmail.com>
2022-06-25 15:17:01 +0100

    (update) suivi du scroll par le filtre pour les grands ecrans

--- resources/js/pages/search.js
+++ resources/js/pages/search.js
@@ -6,27 +6,21 @@ import FetchApi from '../class/FetchApi'
 
 // TODO: ajout d'un panier permettant d'accumuler les prestataire
 
-// const el_search_show_button = document.getElementById('search-show-button')
-
-let FETCH_STATUS = {
-  IDLE: 0,
-  WORKING: 1,
-  DONE: 2
-}
-
-let FETCH_STATE = FETCH_STATUS.IDLE
 
 function init_page() {
-  const searchFilter = document.getElementById('search-filter')
-  const btnShowFilter = document.getElementById('search-show-button')
-  const btnHideFilter = document.getElementById('search-hide-button')
+  const $searchFilter = document.getElementById('search-filter')
+  const $btnShowFilter = document.getElementById('search-show-button')
+  const $btnHideFilter = document.getElementById('search-hide-button')
   const {show, hide} = { show: 'is-show', hide: 'is-hide' } 
 
-  searchFilter.classList.add('is-hide')
+  $searchFilter.classList.add('is-hide')
 
-  btnShowFilter.addEventListener('click', () => searchFilter.classList.replace(hide, show))
+  $btnShowFilter.addEventListener('click', () => {
+    $searchFilter.classList.replace(hide, show)
+    $searchFilter.focus()
+  })
 
-  btnHideFilter.addEventListener('click', () => searchFilter.classList.replace(show, hide))
+  $btnHideFilter.addEventListener('click', () => $searchFilter.classList.replace(show, hide))
 }
 
 init_page()
@@ -34,8 +28,13 @@ init_page()
 FormSelect.init()
 
 Sticky.define({
-  element: '.top-button',
+  element: '#top-button',
   scrollValue: 150,
+})
+
+Sticky.define({
+  element: '#search-filter',
+  scrollValue: 150
 })
 
 
