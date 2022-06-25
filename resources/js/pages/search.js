0806455796347c56923b087f36dc7a1d2abda94a
wuil242 <bousswuil@gmail.com>
2022-06-25 12:02:17 +0100

    (add) filtre chacher et affichable par un boutton

--- resources/js/pages/search.js
+++ resources/js/pages/search.js
@@ -6,6 +6,8 @@ import FetchApi from '../class/FetchApi'
 
 // TODO: ajout d'un panier permettant d'accumuler les prestataire
 
+// const el_search_show_button = document.getElementById('search-show-button')
+
 let FETCH_STATUS = {
   IDLE: 0,
   WORKING: 1,
@@ -14,10 +16,26 @@ let FETCH_STATUS = {
 
 let FETCH_STATE = FETCH_STATUS.IDLE
 
+function init_page() {
+  const searchFilter = document.getElementById('search-filter')
+  const btnShowFilter = document.getElementById('search-show-button')
+  const btnHideFilter = document.getElementById('search-hide-button')
+  const {show, hide} = { show: 'is-show', hide: 'is-hide' } 
+
+  searchFilter.classList.add('is-hide')
+
+  btnShowFilter.addEventListener('click', () => searchFilter.classList.replace(hide, show))
+
+  btnHideFilter.addEventListener('click', () => searchFilter.classList.replace(show, hide))
+}
+
+init_page()
+
 FormSelect.init()
 
 Sticky.define({
   element: '.top-button',
   scrollValue: 150,
 })
+
 
