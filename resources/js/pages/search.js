6a9c8255fba84e3af248f6e46f977993c785dbb8
wuil242 <bousswuil@gmail.com>
2022-06-25 12:02:17 +0100

    (add) filtre chacher et affichable par un boutton

--- resources/js/pages/search.js
+++ resources/js/pages/search.js
@@ -45,6 +45,21 @@ function showComments({target}) {
           })
 }
 
+function init_page() {
+  const searchFilter = document.getElementById('search-filter')
+  const btnShowFilter = document.getElementById('search-show-button')
+  const btnHideFilter = document.getElementById('search-hide-button')
+  const {show, hide} = { show: 'is-show', hide: 'is-hide' } 
+
+  searchFilter.classList.add('is-hide')
+
+  btnShowFilter.addEventListener('click', () => searchFilter.classList.replace(hide, show))
+
+  btnHideFilter.addEventListener('click', () => searchFilter.classList.replace(show, hide))
+}
+
+init_page()
+
 FormSelect.init()
 
 Sticky.define({
